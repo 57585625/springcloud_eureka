48b972b662891bec85609903dbfe18ab4f9d00c4
zy57585625 <623653585@qq.com>
2020-06-30 11:48:34 +0800

    springcloud_eureka 注册中心，可配置集群的注册中心，本人只模仿了单注册中心模式。

---
+++ src/main/java/com/example/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.example.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**springcloud,服务注册中心*/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
